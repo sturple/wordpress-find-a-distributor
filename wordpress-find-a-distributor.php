f723249682bd3db5c8c8b88072db065c6124119d
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-07-01 01:22:27 -0700

    Distributor Meta Boxes: More Meta Boxes & Persist Data

--- wordpress-find-a-distributor.php
+++ wordpress-find-a-distributor.php
@@ -11,31 +11,61 @@
 
 
 require __DIR__ . '/vendor/autoload.php';
-add_action('init',function () {
-    register_post_type('fgms-distributor',[
-        'labels' => [
-            'name' => __('Distributors'),
-            'singular_name' => __('Distributor')
-        ],
-        'public' => true,
-        'register_meta_box_cb' => function (\WP_Post $post) {
-            add_meta_box(
-                'fgms-distributor-info',
-                __('Distributor Information'),
-                function () {
-                    ?>
-                    <label for="fgms-distributor-address"><?php _e('Address'); ?></label>
-                    <br>
-                    <input class="widefat" type="text" name="fgms-distributor-address">
-                    <?php
-                },
-                null,
-                'normal',
-                'default',
-                null
-            );
-        }
-    ]);
+call_user_func(function () {
+    $type='fgms-distributor';
+    $prefix=$type.'-';
+    $addr=$prefix.'address';
+    $city=$prefix.'city';
+    $tu=$prefix.'territorial-unit';
+    $country=$prefix.'country';
+    $mb=$prefix.'info';
+    add_action('init',function () use ($type,$prefix,$addr,$city,$tu,$country,$mb) {
+        register_post_type($type,[
+            'labels' => [
+                'name' => __('Distributors',$type),
+                'singular_name' => __('Distributor',$type)
+            ],
+            'public' => true,
+            'register_meta_box_cb' => function (\WP_Post $post) use ($type,$addr,$city,$tu,$country,$mb) {
+                add_meta_box(
+                    $mb,
+                    __('Distributor Information',$type),
+                    function () use ($type,$addr,$city,$tu,$country,$post) {
+                        $first=true;
+                        $make=function ($name, $title) use ($type,$post,&$first) {
+                            if ($first) $first=false;
+                            else echo('<br>');
+                            ?>
+                            <label for="<?php echo(esc_attr($name)); ?>"><?php _e($title,$type); ?></label>
+                            <br>
+                            <input class="widefat" type="text" name="<?php echo(esc_attr($name)); ?>" value="<?php echo(esc_attr(get_post_meta($post->ID,$name,true))); ?>">
+                            <?php
+                        };
+                        $make($addr,'Address');
+                        $make($city,'City');
+                        $make($tu,'State/Province');
+                        $make($country,'Country');
+                    },
+                    null,
+                    'normal',
+                    'default',
+                    null
+                );
+            }
+        ]);
+    });
+    add_action('save_post',function ($id, \WP_Post $post) use ($type,$addr,$city,$tu,$country) {
+        if ($post->post_type!==$type) return;
+        if (!current_user_can(get_post_type_object($post->post_type)->cap->edit_post,$id)) return;
+        $update=function ($key) use ($id) {
+            $v=isset($_POST[$key]) ? $_POST[$key] : '';
+            update_post_meta($id,$key,$v);
+        };
+        $update($addr);
+        $update($city);
+        $update($tu);
+        $update($country);
+    },10,2);
 });
 
 ?>
